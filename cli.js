fe9be47fea7ae356493525bfad9dfb2a5f2f2d8d
Narottam Kr Jha <narottamkrjha@Narottams-MacBook-Pro.local>
2024-03-05 19:28:06 +0530

    remove comments

--- cli.js
+++ cli.js
@@ -2,17 +2,10 @@
 
 const { program } = require("commander");
 const path = require("path");
-// REMOVE
-// const {
-//   startServer,
-//   injectCodeIntoHtml,
-//   removeInjectedCode,
-// } = require("./util");
 const webpack = require("webpack");
 const webpackDevServer = require("webpack-dev-server");
 const webpackConfig = require("./webpack.config.js");
-// TODO: if we ever allow user to build enable this, mostly not happening
-// const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 
 program
   .option("-d, --directory <dir>", "Directory to host files from")
@@ -36,12 +29,7 @@ program
     webpackConfig.devServer.static.directory = directory;
     webpackConfig.entry = `${ webpackConfig.devServer.static.directory}/${entry}`;
     webpackConfig.devServer.port = port;
-    // not needed as pointed out in the running dev server
-    // webpackConfig.plugins = [
-    //   new webpack.HotModuleReplacementPlugin()
-    // ]
 
-    // set entry point file
 
     const compiler = webpack(webpackConfig);
     const server = new webpackDevServer({...webpackConfig.devServer}, compiler);
@@ -51,63 +39,9 @@ program
       console.log("server starting....");
       await server.start();
     };
-
-    // program.action((a, b, c) => {
-    //   console.log(`a=${a}, b = ${b}, c = ${c}`);
-    // })
-
     runServer();
 
     
   });
 
 program.parse(process.argv);
-
-// const directory = program.directory || "public";
-// const htmlFileName = program.html || "index.html";
-// const port = program.port || 3001;
-
-// console.log(`the port is + ${port} + directory = ${program.directory}`);
-
-// webpackConfig.devServer.static.directory = path.join(__dirname, directory);
-// webpackConfig.devServer.port = port;
-// console.log(webpackConfig);
-// webpackConfig.resolve.alias.src =  path.join(__dirname, directory);;
-// HTML webpack plugin
-// TODO: if we ever allow user to build enable this
-// webpackConfig.plugins = [...webpackConfig.plugins, new HtmlWebpackPlugin({
-//   template: `/${directory}/${htmlFileName}`,
-//   filename: `${htmlFileName}`
-// })]
-
-// REMOVE
-// program
-//   // in this way we can take multiple space seperated
-//   // arguments from the user
-//   .command("serve [dir] [port] [<html-file-path>]")
-//   .action((dir, port, htmlFileName) => {
-//     // dir is either a relative path or an absolute path
-//     // if its relative then it is w.r.t to __dirname
-//     startServer(port, dir, htmlFileName);
-//     // inject code for socket io
-//     // with the given html file
-//     // TODO: operating system dependent
-//     //    const absolutePath = path.join(__dirname, dir, htmlFileName)
-//     console.log("0." + dir + "/" + htmlFileName);
-//     !path.isAbsolute(dir) && (dir = path.join(__dirname, dir));
-//     const joinedPathWithFileName = path.join(dir, htmlFileName);
-//     injectCodeIntoHtml(joinedPathWithFileName);
-//     const cleanupCb = () => {
-//         try {
-//             removeInjectedCode(joinedPathWithFileName);
-//             process.exit(0)
-//         } catch (error) {
-//             console.error("Error during cleanup:", error);
-//             process.exit(1)
-//         }
-
-//     }
-//     process.on("SIGINT", cleanupCb );
-//     process.on("exit", cleanupCb)
-//     // should run when user closes the cli app
-//   });
