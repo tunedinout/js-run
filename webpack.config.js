f539aa10f52658ece775649f9ab8119f27160b9d
Narottam Kr Jha <narottamkrjha@Narottams-MacBook-Pro.local>
2024-03-05 19:31:31 +0530

    remove comments

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
-    // entry: './.js',/
-    // useful when one builds
-    // TODO: if we ever support building files, but mostly wont
+    // entry point is set at the time of script start
     output: {
         path: path.resolve(__dirname,  'public'),
         filename: "bundle.js",
@@ -23,18 +21,6 @@ module.exports = {
           },
     },
     stats: 'minimal',
-    // resolve: {
-    //     alias: {
-    //       src:  path.join(__dirname, 'public'),
-    //     },
-    //   },
     plugins: [
-        // TODO: insert this dynamically
-        // new HtmlWebpackPlugin({
-        //     // what about this ???
-        //     // content base and html file nam
-        //     template: './public/index.html',
-        //     filename: 'index.html'
-        // })
     ]
 }
